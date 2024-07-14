00895a5a9c9ebfbb726d70c6360cb3a0625912a0
gpanushka <167328539+gpanushka@users.noreply.github.com>
2024-07-14 17:50:14 +0530

    Update and rename task1.java to NumberGuessingGame.java

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 
-public class NumberGuessing{
+public class NumberGuessingGame {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rd = new Random();
